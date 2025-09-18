e044aa7a68ccc4b735de929a571f78cb6ff863bf
Skazka1 <125551509+smoozeboy@users.noreply.github.com>
2025-09-18 19:47:56 +0500

    ready

--- OOP/item.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-struct Item
-{
-	Item* prev;
-	Item* next;
-};
--- OOP/list.h
+++ OOP/list.h
@@ -1,7 +1,23 @@
 #pragma once
-#include "item.h"
+#include "stdio.h"
+#include "stdlib.h"
+struct Item
+{
+	struct Item* prev;
+	struct Item* next;
+};
 struct List
 {
-	Item* tail;
-	Item* head;
+	struct Item* tail;
+	struct Item* head;
 };
+
+void Add(struct List*, struct Item*);
+void Delete(struct List*, const int);
+struct Item* GetItem(const struct List*, const int);
+struct Item* Remove(struct List*, const int);
+void Insert(struct List*, struct Item*, const int);
+int Count(const struct List*);
+void Clear(struct List*);
+int GetIndex(const struct List*, const struct Item*);
+void PrintList(const struct List*);
